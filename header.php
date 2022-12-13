a53580c4fa6f1842384c0b455b793725c4b4e456
jean-derieppe <jean.derieppe@laplateforme.io>
2022-12-13 19:21:46 +0100

    en cour

---
+++ header.php
@@ -0,0 +1,28 @@
+<header>
+
+    <h1 class="Module">H.F.A Location</h1>
+    
+    <nav class="header">
+        <ul class="list1">      
+
+                <a href="index.php">
+                <button class="btn">
+                Accueil
+                </button>
+                </a>
+
+                <a href="inscription.php">
+                <button class="btn">
+                Inscription
+                </button>
+                </a>
+
+                <a href="connexion.php">
+                <button class="btn">
+                Connexion
+                </button>
+                </a>   
+        </ul>
+        <hr>
+    </nav>
+</header>
